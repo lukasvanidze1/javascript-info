3bb7c4e51f692ef96afbb9826301fad10cf69f40
lukasvanidze1 <lukasvanidzee@gmail.com>
2020-03-27 17:32:37 +0400

    sheckLogin

---
+++ JavaScript Fundamentals/If-Else/checkLogin.js
@@ -0,0 +1,34 @@
+'use strict'
+
+let userName = prompt("What is you name?", "");
+
+if (userName == "Luka") {
+    let vipPass = prompt("Ohh Vip, Please Tell Me The password", "")
+
+    if (vipPass == "MR-Luca") {
+        alert("Welcome boss");
+    }
+    else if (vipPass == "" || vipPass == null) {
+        alert("You are not Vip any more");
+    }
+    else ("Sorry mister you are not in Vip list")
+}
+else if (userName == "Admin") {
+    let pass = prompt("tell me password", "");
+
+    if (pass == "TheMaster") {
+        alert("Welcome bro");
+    }
+    else if (pass == "" || pass == null) {
+        alert("You Canceled");
+    }
+    else {
+        alert("Wrong password");
+    }
+}
+else if (userName == "" || userName == null) {
+    alert("You Canceled");
+}
+else {
+    alert("I don't know you");
+}
